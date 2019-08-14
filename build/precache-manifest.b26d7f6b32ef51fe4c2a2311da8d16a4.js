ec3acb137583e2c4c3680395d9fa434b6f19966a
Christian Jess Mark <christianjessmark@gmail.com>
2019-08-14 09:46:37 +0200

    create build, connect to MLab

---
+++ build/precache-manifest.b26d7f6b32ef51fe4c2a2311da8d16a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c2053e86e27eb51cce1837126da0c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11f0c2ef3fb16860194",
+    "url": "/static/css/2.a73d7da8.chunk.css"
+  },
+  {
+    "revision": "a11f0c2ef3fb16860194",
+    "url": "/static/js/2.423583c9.chunk.js"
+  },
+  {
+    "revision": "4e7711103ce13d202c54",
+    "url": "/static/js/main.7a21bfab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
